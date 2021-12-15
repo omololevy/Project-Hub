9517f18a7ad6f88471fc609b47ec70a28d3fc684
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-12-16 01:43:52 +0300

    Create the filr that restricts the access of files

---
+++ proHub/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
